6052d690d3060de3b393b446982af6fa3b948456
Marissa <Marissa.Bart@cuw.edu>
2017-02-03 18:16:36 -0600

    Added revisions to game

--- DiceGameRev1.2.js
+++ DiceGameRev1.2.js
@@ -1,14 +1,8 @@
 "use strict";
-function getUserBet(){
-	var betOptions = ["Win ", "Lose ", "TIE ", "Skip Bet "];
-	var userBetSelection = prompt("Do you think you will: "+betOptions+" \n Correct: add $13.00 \n Incorrect: deduct $6.50 \n TIE: Correct- automatically win! Incorrect- deduct $13.00 \n Skip Bet: deduct $3.25");
-	return userBetSelection;
-}
-
-function getRandomBet (betOptions){
-	var betOptions = ["Win ", "Lose ", "TIE ", "Skip Bet "];
-	var computerBetSelection = Math.floor((Math.random()* betOptions.length >> 0));
-	return computerBetSelection;
+function getUserGuess(){
+	var guessOptions = ["Win ", "Lose ", "Tie "];
+	var userGuessSelection = prompt("Do you think you will: "+guessOptions+" on this round?");
+	return userGuessSelection;
 }
 
 function rollDie(numberofSides){
@@ -48,47 +42,29 @@ function determineWinner(player1Score, player2Score) {
 	}
 }
 
-//ver 1.3
-	
-function walletMonitor(){
-		var walletStart = $13.00;
-		var walletMax = $52.00; 
-		//while (walletStart = 0>= walletMax, walletTotal = < walletMax){
-		while (player1Wallet !== walletMax && player2Wallet !== walletMax) {
-			//play();
-		alert("Insufficent funds for Player " has caused the game to stop, play again!");
+//ver 1.2
+function compareGuess (userGuessSelection){
+	var player1Guess = getUserGuess(userGuessSelection);
+	var player1Score = determineWinner(player1Score);
+	var player2Score = determineWinner(player2Score);
+	var guessOptions = ["Win ", "Lose ",  "Tie "];
+	var win = 0;
+	var lose = 1;
+	var tie = 2;
+	if (userGuessSelection === 0 && userGuessSelection === determineWinner(player1Score, player2Score)){
+		alert("You were right!");
+	}else if(userGuessSelection === 1 && userGuessSelection === determineWinner(player1Score, player2Score)){
+		alert("You were right!");
+	}else if(userGuessSelection === 2 && userGuessSelection === determineWinner(player1Score, player2Score)){
+		alert("You were right!");
+	}
 }
 
-//function playerWalletBalance(){
-	//var walletTotal = $13;
-	//var Win = $13.00;
-	//var Lose = $6.50;
-	//var Tieyay = autoWin;
-	//var Tieno = $13.00
-	//var Pass = $3.25; 
-	//if win = (walletTotal + Win);
-	//else if lose = (walletTotal - Lose);
-	//else if Tie
-		//if correct = tieCorrectChoice();
-		//else if incorrect = (walletTotal - Tieno);
-	//else if pass = (walletTotal - Pass);
-	//else please select from the options above.
-//} 
-	
-
-//function tieCorrectChoice ()}
-//Tieyay = autoWin alert("Player " was correct to select TIE, play again!");
-//}
-
 function runStartGame(){
-	var userBetSelection = getUserBet();
+	var userGuessSelection = getUserGuess();
 	var player1Score = determineScore();
 	var player2Score = determineScore();
 	alert ("Player 1 Rolled " +player1Score+ " Player 2 Rolled " +player2Score);
 	determineWinner(player1Score, player2Score);
 }
 
-//var testWinner = determineWinner();
-//console.log(testWinner);
-
-runStartGame();
